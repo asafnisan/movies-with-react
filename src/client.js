d1c262124ab3a15c96154dc7ac60f80006bac433
Asaf Nisan Guler <asafnisan@gmail.com>
2018-11-22 19:03:08 +0300

    request to be sent over https, instead of http

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 // client
 function client() {
     function getMovies(search,success,pageNum) {
-        const apiEndpoint = 'http://www.omdbapi.com/?apikey=aebd27fd';
+        const apiEndpoint = 'https://www.omdbapi.com/?apikey=aebd27fd';
         const query = apiEndpoint + '&s=' + search + '&page=' + pageNum;
         const movies = fetch(query).then(function(response){
             return response.json()
@@ -10,7 +10,7 @@ function client() {
         })
     }
     function getMovie(movieID,success){
-        const apiEndpoint = 'http://www.omdbapi.com/?apikey=aebd27fd';
+        const apiEndpoint = 'https://www.omdbapi.com/?apikey=aebd27fd';
         const movieCall = apiEndpoint + '&i=' + movieID
         const movie = fetch(movieCall).then(function(response){
             return response.json()
